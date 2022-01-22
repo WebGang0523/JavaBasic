8e7b168829b7c38083508ba3cd41dc6b93467dcd
xuchenggang <chenggang.xu@xipu.com>
2022-01-22 14:31:27 +0800

    ..

--- src/com/xucg/sort/SimpleInsertSort.java
+++ src/com/xucg/sort/SimpleInsertSort.java
@@ -6,7 +6,8 @@ public class SimpleInsertSort {
 
     public static void main(String[] args) {
         int[] arr = {8,9,1,7,2,3,5,4,6,0};
-        simpleInsertSort(arr);
+//        simpleInsertSort(arr);
+        simpleInsertSort1(arr);
     }
 
     public static void simpleInsertSort(int[] arr) {
@@ -27,5 +28,21 @@ public class SimpleInsertSort {
         }
 
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void simpleInsertSort1(int[] arr){
+        for (int i=1;i< arr.length;i++) {
+            for(int j=i-1;j>=0 && arr[j] > arr[j+1]; j--) {
+                swap(arr,j,j+1);
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 }
